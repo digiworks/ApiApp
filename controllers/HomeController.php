42c5fea619578cd29e69d8afd86d37a80fc18533
stefano.cavazzini <digiworkscom@gmail.com>
2022-04-28 15:01:08 +0200

    Update HomeController.php

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -19,11 +19,14 @@ class HomeController extends AppController {
      */
     public function home(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface {
 
-        $currentView = 'js/views/user/signin.js';
-        $renderManager = ApiAppFactory::getApp()->getService(ServiceTypes::RENDER);
-        $response->getBody()->write($renderManager->getRender()->useTheme("login")->renderView($currentView));
-
-        return $response;
+        try {
+            $currentView = 'js/views/user/signin.js';
+            $this->setRequest($request)->setResponse($response)->setCurrentView($currentView)->buildViewResponse()->render();
+        } catch (Exception $ex) {
+            ApiAppFactory::getApp()->getLogger()->error("error", $ex->getMessage());
+            ApiAppFactory::getApp()->getLogger()->error("error", $ex->getTraceAsString());
+        }
+        return $this->getResponse();
     }
 
 }
