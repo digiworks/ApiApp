0f26315700eaa371e3f7650a6b22cd4992636803
stefano.cavazzini <digiworkscom@gmail.com>
2022-05-03 14:34:58 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -10,7 +10,7 @@ use code\service\ServiceTypes;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 
 $app = ApiAppFactory::create();
